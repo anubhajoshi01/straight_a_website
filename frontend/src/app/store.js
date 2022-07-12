6cd441fe9b578e8eea85a2d8faf8dc391a2c39cf
anubha <anubhajoshi01@gmail.com>
2022-07-12 15:11:45 -0400

    blog slice and service functions

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import formReducer from '../features/forms/formSlice'
 import authReducer from '../features/auth/authSlice'
+import blogReducer from '../features/blog/blogSlice'
 
 export const store = configureStore({
   reducer: {
     forms: formReducer,
-    auth: authReducer
+    auth: authReducer,
+    blogs: blogReducer
   },
 });
