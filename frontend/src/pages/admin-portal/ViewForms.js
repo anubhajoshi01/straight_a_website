1beeb1dcadf315c924730924e6a1ecaa5e072200
Lele660 <60441401+Lele660@users.noreply.github.com>
2022-07-13 15:39:52 -0600

    attempt 2

--- frontend/src/pages/admin-portal/ViewForms.js
+++ frontend/src/pages/admin-portal/ViewForms.js
@@ -1,41 +1,61 @@
 import FormResponseCard from "../../components/FormResponseCard"
-
-import { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { getForms, reset } from '../../features/forms/formSlice'
 
 
 function ViewForms() {
-    console.log('in view forms')
+    //console.log('in view forms')
 
     const dispatch = useDispatch()
+    //const [displayList, setDisplayList] = useState([])
 
     const { forms, isLoading, isError, message } = useSelector(
-    (state) => state.forms
-  )
+        (state) => state.forms
+    )
 
-  useEffect(() => {
-    if (isError) {
-      console.log(message)
-    }
+    useEffect(() => {
+        if (isError) {
+            console.log(message)
+        }
 
-    dispatch(getForms())
-    console.log(forms)
-    
-    return () => {
-      dispatch(reset())
-    }
+        dispatch(getForms())
+        console.log(forms[0])
+        //setDisplayList(forms)
+        return () => {
+            dispatch(reset())
+        }
 
-    
 
-  }, [forms, isError, message, dispatch])
+    }, [forms, isError, message, dispatch])
 
+    // forms.map((item) => (
+    //     <li>
+    //         <FormResponseCard timestamp={""} studentName={item.name} parentName={item.parentName}
+    //         phone={item.phone} email={item.email} school={item.school}  />
+    //     </li>
+    // ))
 
     return (
+        /*
+        <div>
+            <ul>
+            {
+                <li>
+                       <FormResponseCard timestamp={""} studentName={forms[0].studentName} parentName={forms[0].parentName} grade = {forms[0].grade} comments = {forms[0].more}
+                       phone={forms[0].phone} email={forms[0].email} school={forms[0].school} />
+                </li>
+            }
+            </ul>
+        </div>
+        */
+        
+        
         <div>
             <FormResponseCard timestamp={'00/00/0000 00:00'} studentName={'John Doe'} parentName={'Jane Doe'} phone={'000-000-0000'} email={'email@gmail.com'} school={'School A'} 
             grade={'00'} comments={'hujiwh hueiwl hui huwql nms,a hfju; qi; hjk hjkdl huiql ueil djkal hghjfkgrf hjk;fjkd.h jvkf,owptig uipew uifld hvjkfpwout ihfjuil huigtp whugi pwhvui uhwg'} resolved={false}/>
         </div>
+        
     )
 }
 
