3040f0e944387e9c1854bc6bf36dc36d47ff0c51
Lele660 <60441401+Lele660@users.noreply.github.com>
2022-07-26 09:49:38 -0600

    probono

---
+++ frontend/src/pages/website/about-pages/ProBono.js
@@ -0,0 +1,27 @@
+import Header from '../../../components/Header'
+import Footer from '../../../components/Footer'
+
+
+
+
+
+
+function ProBono(){
+
+
+
+    return (
+        <>
+        <Header />
+        
+        <Footer />
+        
+        
+        
+        </>
+
+
+    )
+}
+
+export default ProBono
