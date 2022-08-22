bf3ce88eaa9a0ebd99d886a436bd79779b353280
Lele660 <60441401+Lele660@users.noreply.github.com>
2022-08-22 14:01:11 -0600

    frontend

--- frontend/src/pages/admin-portal/Login.js
+++ frontend/src/pages/admin-portal/Login.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Spinner from '../../components/Spinner';
 import { login, reset } from '../../features/auth/authSlice';
-import './Login.css'
+
 
 function Login() {
   
